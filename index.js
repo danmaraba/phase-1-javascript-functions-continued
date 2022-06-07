c30b5be20067c4f93696cb1d0cf7f0daeda95066
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-07 19:19:23 +0300

    Done with the asignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(task='roller-skate'){
+    return (`This Saturday, I want to ${task}!`);
+}
+
+function mondayWork(task='go to the office'){
+    return (`This Monday, I will ${task}.`)
+}
+
+function wrapAdjective(result="*"){
+    return function wrapAdj(emphatic="a hard worker"){
+    return `You are ${result}${emphatic}${result}!`;
+    }
+}
+
+
